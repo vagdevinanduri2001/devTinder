ab68e4031fbf18fe8f64c8cc9fa14a4f0b0d9d90
vagdevinanduri2001 <bhuvanananduri2001@gmail.com>
2026-06-29 14:50:30 +0530

    Error-Handling

--- src/app.js
+++ src/app.js
@@ -5,56 +5,26 @@ const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express();
 
-// Multiple route Handlers
-// rH --> route Handler
-// The functions in the middle are called as middlewares
-// and the functions which are actually sending back the response
-// i.e., res.send() are the request/route/response handler.
 
-//GET /users => middleware chain => request handler
-
-
-//Handle all auth middleware
-// We can also use GET, POST, DELETE, etc.
-app.use("/admin", adminAuth);
-
-app.use("/admin/getAllData", (req, res) => {
-    // logic to check if the user is authorized
-
-    res.send("All data sent!");
-});
-
-app.use("/admin/deleteUser", (req, res) => {
-    // logic to check if the user is authorized
-
-    res.send("User deleted!");
-});
-
-
-//-----------------------------------------------------------------
-
-//since we have only one path of /user, we can write as below.
-app.use("/user", userAuth, (req, res, next) => {
-    //This is a route Handler.
-    console.log("Response 1");
-    // res.send("Response 1");
-    next();
-},
-(req, res, next) => {
-    console.log("Response 2");
-    // res.send("Response 2");
-    next();
-    },
-    (req, res) => {
-        //This is a route Handler.
-        console.log("Response 3");
-        res.send("Response 3");
-    }
-);
-
-//In this case, we don't need auth, so we are skipping userAuth here.
+//Always use try-catch for better error handling.
 app.use("/user/login", (req, res) => {
-    res.send("Logged in!");
+
+    try {
+        throw new Error("I don't know what error");
+        res.send("User data sent!");
+    } catch (e) {
+        res.status(500).send("Some Error!!");
+    }
+    // throw new Error("I don't know what error");
+    // res.send("User data sent!");
 });
+
+
+//Wildcard error handling
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        res.status(500).send("Something went Wrong!!");
+    }
+})
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
